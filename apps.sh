3824561f985b2ee6eeeededfe8bf19af84317bc4
moby1988 <105594995+moby1988@users.noreply.github.com>
2022-06-13 17:35:09 +0300

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade
-
-
-# Telegram
-
-sudo apt install telegram
+sudo apt update && sudo apt upgrade -y
 
 # Snap
 
 sudo rm /etc/apt/preferences.d/nosnap.pref
 sudo apt update
-sudo apt install snapd
+sudo apt install snapd -y
+sudo snap install core
+sudo snap install snap-store
+sudo apt update
+
+# Bluemail
+
+sudo snap install bluemail
 
 # Spotify
 
-sudo snap install spotify -y
+sudo snap install spotify
 
 # install hardware info
 
@@ -23,19 +25,11 @@ sudo apt install hardinfo -y
 
 # install pycharm
 
-yes | sudo snap install pycharm-professional --classic
+sudo snap install pycharm-professional --classic -y
 
 # install vim
 
-yes | sudo apt install vim
-
-# install atom
-
-wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
-sudo apt-get update
-
-yes | sudo apt install atom
+sudo apt install vim -y
 
 # Wine
 
@@ -45,7 +39,8 @@ sudo mv winehq.key /usr/share/keyrings/winehq-archive.key
 wget -nc https://dl.winehq.org/wine-builds/ubuntu/dists/focal/winehq-focal.sources
 sudo mv winehq-focal.sources /etc/apt/sources.list.d/
 sudo apt update
-sudo apt install --install-recommends winehq-stable
+sudo apt install --install-recommends winehq-stable -y
+winecfg
 
 # install google chrome
 
@@ -54,20 +49,39 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 # other stuff
 
-sudo apt install unrar
+sudo apt install unrar -y
 
 sudo apt install python3-pip -y
+sudo apt install python3-tk -y
+
 
 sudo apt update
 
-sudo apt  install links -y
+# Ksnip
+sudo add-apt-repository ppa:nemonein/ksnip
+sudo apt update
+sudo apt install ksnip -y
+
+#Viber
+sudo apt update
+sudo dpkg -i viber.deb
 
 
 # Visual Studio Code 
 
+#cd /Downloads
+#sudo apt update
+#sudo dpkg -i ./code_1.67.2-1652812855_amd64.deb
 
+# Telegram
 
+#sudo apt install telegram
 
-cd /Downloads
-sudo apt update
-sudo apt install ./code_1.67.2-1652812855_amd64.deb
+#sudo apt  install links -y
+
+# install atom
+
+#wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+#sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+#sudo apt-get update
+#yes | sudo apt install atom
